cd375bd05b4087c58627fa971a12683e0229709e
Joyce Silva <joyce@integra.do>
2023-02-12 18:21:32 -0300

    fix: change file url

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -8,7 +8,7 @@ const Introduction = () => {
       <div className="Introduction__Content">
         <h1>Joyce Silva</h1>
         <p>Fullstack Web Developer</p>
-        <DownloadButton file="https://lime-cynthia-56.tiiny.site" />
+        <DownloadButton file="http://dl.dropboxusercontent.com/s/1zprloiqiaobwod/document.pdf" />
       </div>
     </section>
   );
